5a5c85f482eee3850c5c72f3c755591196aaa353
vanessa-wambui <vanessawambui251@gmail.com>
2026-02-04 09:59:06 +0300

    first commit

---
+++ lesson1a.py
@@ -0,0 +1,29 @@
+# what is a variable:labels which act as containers to hold values.In python a variable is always associated with some values . 
+# We use the assignment operater(=) to assign values to a variable
+# anything inside of quotes in python is called a string
+message = "Hello there!Hope everything is fine"
+print(message)
+
+
+
+message2 = " Goodbye"
+print(message2)
+
+
+# concatenating variable(Joining two or more outputs together)
+message3 = message + message2
+print(message3)
+
+
+# varibles can be named using a letter, a number and an underscore. They can start with a letter and an underscore and letter and not number
+name_1="Brendah"
+print(name_1)
+
+
+
+# variable names cannot contain space we can use the underscore to separate the names
+total_marks=85
+print(total_marks)
+
+
+# variable names are not supposed to be keyword/reserved characters in python
---
+++ test.py
@@ -0,0 +1,5 @@
+print("Hello Bobo")
+
+# this is a comment
+"""this is another comment"""
+'''This is another docstring'''
