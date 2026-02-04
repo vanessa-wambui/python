85cf194aa71ee5a4f99eb8182e56bc46d95e72a9
vanessa-wambui <vanessawambui251@gmail.com>
2026-02-04 11:22:37 +0300

    add python code

---
+++ assignment1.py
@@ -0,0 +1,24 @@
+# Write a python program to calculate the BMI of a person whose weight is 70 kg and height is 1.74. BMI= (weight / height squared)
+
+# find th area and perimeter of a rectangle whose 48cm and width is 25cm
+
+weight = 70
+height = 1.74 ** 2
+BMI = weight / height
+print("The BMI is:" , BMI)
+
+length = 48
+width = 25
+area = length * width
+print("The are of the rectangle is:", area)
+
+
+length = 48 * 2
+width = 25 * 2
+perimeter = length + width
+print("The perimeter of the rectangle is" , perimeter)
+
+# python list is a way to store multiple items in one variable for example ordered list in which the items are kept in their position, changeable lists in which you can add remove and edit items and allow duplicate lists in which some items appear more than once.
+
+# dictionary data type use to store data in key value pairs.
+
---
+++ lesson1d.py
@@ -0,0 +1,11 @@
+# create a python program that is able to calculate the simple interes given the princiole as 50000, rate as 5% and time as 8 years. (p*r*t)/100
+principle = 50000
+rate = 5
+time = 8
+simpleinterest = (principle * rate * time)/100 
+print("The simple interest is:" , simpleinterest)
+
+#Write a python program to calculate the BMI of a person whose weight is 70 kg and height is 1.74. BMI= (weight / height squared)
+
+# find th area and perimeter of a rectangle whose 48cm and width is 25cm
+
