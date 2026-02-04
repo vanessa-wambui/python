c37cbfcb62627f43cb97a07a2780e97720ecf76f
vanessa-wambui <vanessawambui251@gmail.com>
2026-02-04 10:15:58 +0300

    add python file

---
+++ lesson1b.py
@@ -0,0 +1,18 @@
+# Python Data types
+# string-it is a series of characters that are inside of quotes
+name = "Joseph"
+print(name)
+
+
+# numeric
+# int-this indicates a whole number in python. for an integer we dont use quote
+age = 39
+print("The age of a person is:" , age)
+print(type(age))
+
+# float-this is a number with a decimal
+coordinates = 15.4524
+print("The coordintes are:" , 15.4524)
+print(type(15.4524))
+
+# research and come up with a coplex data type
