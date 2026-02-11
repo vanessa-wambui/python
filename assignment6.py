cbfa8a422a12033e829b30d179c071029da40b57
vanessa-wambui <vanessawambui251@gmail.com>
2026-02-11 11:02:03 +0300

    add code

---
+++ assignment6.py
@@ -0,0 +1,12 @@
+# a database is a place where data is stored in an organized way so it can be easily accessed.
+# how to create a database 
+# 1. open localhost/phpmyadmin
+# 2.click on new and enter the name of database ,click create
+# how to create a table
+# 1. open the database
+# 2. click on create table, insert the name of your table, and the number of columns
+# 3. click on create and insert the names of each column ,the lengths and click on go
+# How to insert records
+# 1. click on the insert tab 
+# 2. insert data manually
+# 3. when done,click go and go to the browser tab 
